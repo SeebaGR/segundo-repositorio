a12bd1819a810730005130316929fcf145c305fa
Sebastian Godoy <s.godoy.rubio@gmail.com>
2021-03-19 17:28:13 -0300

    segundo repositorio

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBLoyUIZUtJMGxzfSFgtpXek_cWhhKVzow",
+    authDomain: "mapudung.firebaseapp.com",
+    projectId: "mapudung",
+    storageBucket: "mapudung.appspot.com",
+    messagingSenderId: "439567609871",
+    appId: "1:439567609871:web:687f63b240e0750573f37a"
+  }
+  firebase.initializeApp(firebaseConfig);
+  
